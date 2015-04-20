3c93dd319d967b7f4a3aa1018ddd9da0c4a3eea5
DeviateFish <savorydeviatedelight@gmail.com>
2015-04-20 14:05:12 -0700

    rebuilding

--- dist/ingress-model-viewer.js
+++ dist/ingress-model-viewer.js
@@ -491,7 +491,8 @@ var constants = {
     ArtifactHelios: 'ArtifactHeliosTexture',
     ArtifactAmar: 'ArtifactAmarTexture',
     ArtifactJarvis: 'ArtifactJarvisTexture',
-    ArtifactShonin: 'ArtifactShoninTexture'
+    ArtifactShonin: 'ArtifactShoninTexture',
+    ArtifactLightman: 'ArtifactLightmanTexture'
   }
 };
 
