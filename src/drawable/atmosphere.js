15119ccba98663cdc56a27c9443886b9f069d0d2
DeviateFish <savorydeviatedelight@gmail.com>
2015-08-29 22:52:51 -0700

    Adding documentation, part 1
    Basically starting from the top.

--- src/drawable/atmosphere.js
+++ src/drawable/atmosphere.js
@@ -5,10 +5,22 @@ import { mat3, mat4 } from 'gl-matrix';
 
 const PROGRAM = Constants.Program.Atmosphere;
 
-// this program (atmosphere.glsl.vert and atmosphere.glsl.frag)
-// is a modified version of the atmosphere program in
-// https://github.com/dataarts/webgl-globe/blob/master/globe/globe.js
+/**
+ * This is a modified version of the atmosphere program from:
+ * https://github.com/dataarts/webgl-globe/blob/master/globe/globe.js
+ */
 class AtmosphereDrawable extends ModelDrawable {
+
+  /**
+   * Initializer
+   * @param  {Number} radius      Radius of the world.
+   *                              This should match the radius of the world mesh the
+   *                              atmosphere is being rendered over.
+   * @param  {Number} vSlices     Number of vertical slices for the sphere mesh
+   * @param  {Number} hSlices     Number of horizontal slices for the sphere mesh
+   * @param  {Number} scaleFactor The percent to scale the mesh
+   * @return {void}
+   */
   constructor(radius, vSlices, hSlices, scaleFactor) {
     super(PROGRAM, null);
     this.radius = radius;
@@ -19,13 +31,29 @@ class AtmosphereDrawable extends ModelDrawable {
     mat4.scale(this.local, this.local, [this.scaleFactor, this.scaleFactor, this.scaleFactor]);
   }
 
+  /**
+   * Updates the view matrices of the model
+   *
+   * @chainable
+   * @see    src/drawable/model.js#updateView
+   * @param  {mat4} viewProject   combined projection matrix multiplied by view matrix.
+   * @return {this}
+   */
   updateView(viewProject) {
     super.updateView(viewProject);
-     var invert = mat4.invert(mat4.create(), viewProject),
-         transpose = mat4.transpose(mat4.create(), invert);
+    var invert = mat4.invert(mat4.create(), viewProject),
+        transpose = mat4.transpose(mat4.create(), invert);
     this.uniforms.u_normalMatrix = mat3.fromMat4(mat3.create(), transpose);
+    return this
   }
 
+  /**
+   * Initializes the drawable
+   *
+   * @see    src/drawable.js
+   * @param  {AssetManager} manager The AssetManager containing the required assets.
+   * @return {boolean}
+   */
   init(manager) {
     this.mesh = new SphereMesh(
       manager._gl,
--- src/drawable/bicolored.js
+++ src/drawable/bicolored.js
@@ -4,13 +4,34 @@ import { vec4 } from 'gl-matrix';
 
 const PROGRAM = Constants.Program.Bicolored;
 
-// default quality color: very rare
+/**
+ * Default quality color.
+ * @type {vec4}
+ */
 const defaultColor0 = vec4.clone(Constants.qualityColors.VERY_RARE);
 
-// default glow color: xm color
+/**
+ * Default glow color
+ * @type {vec4}
+ */
 const defaultColor1 = vec4.clone(Constants.xmColors.coreGlow);
 
+/**
+ * This is used for items and other renderables that have two visible colors
+ *
+ * The specifics of it are basically: if the texture has an opacity less than 0.5,
+ * the texture color is blended with u_color0
+ * Otherwise, it's the texture color blended with u_color1
+ *
+ * Or something like that.
+ */
 class BicoloredDrawable extends TexturedDrawable {
+
+  /**
+   * Initialized a bi-colored drawable
+   * @param  {String} meshName    Internal name of the mesh for this drawable
+   * @param  {String} textureName Internal name of the texture for this drawble
+   */
   constructor(meshName, textureName) {
     super(PROGRAM, meshName, textureName);
     this.uniforms.u_color0 = vec4.clone(defaultColor0);
